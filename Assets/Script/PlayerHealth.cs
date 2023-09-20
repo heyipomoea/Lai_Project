3bf162b5970a25122c7a603fb945436a2df53267
heyipomoea <heyipomoea@gmail.com>
2023-09-20 11:01:53 +0800

    Update UI血條

---
+++ Assets/Script/PlayerHealth.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private GameObject ref_FX;
+    [SerializeField] private float maxHP = 10f;
+    public float MaxHP => maxHP;
+    private float hp;
+    public float CurrentHP => hp;
+    private float AfterDeadTime = 3f;
+    private bool isDead = false;
+    public bool IsDead => isDead;
+
+    private EventHandler deadReceived;
+    public event EventHandler DeadReceived
+    {
+        add { deadReceived += value; }
+        remove { deadReceived -= value; }
+    }
+
+    private EventHandler hitReceived;
+    public event EventHandler HitReceived
+    {
+        add { hitReceived += value; }
+        remove { hitReceived -= value; }
+    }
+
+    private EventHandler revivalReceived;
+    public event EventHandler RevivalReceived
+    {
+        add { revivalReceived += value; }
+        remove { revivalReceived -= value; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = MaxHP;
+    }
+
+    public void OnDamage(float damage)
+    {
+        hitReceived?.Invoke(this, EventArgs.Empty);
+        hp -= damage;
+        if (hp <= 0 && !isDead) { OnDead(); }
+    }
+
+    public void OnHealing(float heal)
+    {
+        hp += heal;
+        if(hp >= MaxHP) { hp = MaxHP; }
+    }
+    
+    void OnDead()
+    {
+        isDead = true;
+
+        deadReceived?.Invoke(this, EventArgs.Empty);
+        enabled = false;
+        StartCoroutine(AfterDead());
+    }
+
+    IEnumerator AfterDead()
+    {
+        yield return new WaitForSeconds(AfterDeadTime);
+        GameObject tempFx = Instantiate(ref_FX, transform.position, Quaternion.identity);
+        tempFx.AddComponent<ParticleEffectController>();
+
+        OnRevival();
+    }
+
+    public void OnRevival()
+    {
+        isDead = false;
+        enabled = true;
+        revivalReceived?.Invoke(this, EventArgs.Empty);
+        hp = MaxHP;
+    }
+}
